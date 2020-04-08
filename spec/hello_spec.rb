c87fe31db20fa01dd11dc4ab463bef434bcd9d08
Jody Vandenschrick <jody.vandenschrick@cultureamp.com>
2020-04-08 13:52:00 +1000

    Add tests to the gem

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -3,7 +3,15 @@ RSpec.describe Hello do
     expect(Hello::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "respond to english" do
+    expect(Hello.hi("english")).to eq("hello world")
+  end
+
+  it "respond to spanish" do
+    expect(Hello.hi("spanish")).to eq("hola mundo")
+  end
+
+  it "has a default" do
+    expect(Hello.hi("test")).to eq("hello world")
   end
 end
